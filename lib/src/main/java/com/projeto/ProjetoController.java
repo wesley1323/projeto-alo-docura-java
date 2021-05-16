bc0b2686f3e0af509bedd0a3e3a225a4921aaad7
PedroSalvino <pedro_luiz7199@outlook.com>
2021-05-16 12:10:18 -0300

    Adicionando o cadastro do cliente

--- lib/src/main/java/com/projeto/ProjetoController.java
+++ lib/src/main/java/com/projeto/ProjetoController.java
@@ -1,10 +1,24 @@
 package com.projeto;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.model.Cliente;
+import com.model.ClienteService;
+
 
 @Controller
+@ComponentScan("com.model")
 public class ProjetoController {
+	@Autowired
+	private ApplicationContext context;
+	
 	@GetMapping("/")
 	public String home() {
 		return "index";
@@ -16,12 +30,21 @@ public class ProjetoController {
 	}
 	
 	@GetMapping("/delivery")
-	public String delivery() {
+	public String formCliente(Model model) {
+		model.addAttribute("cliente",new Cliente());
 		return "delivery";
+	}
+	
+	@PostMapping("/pedido")
+	public String postCliente(@ModelAttribute Cliente cli, Model model) {
+		ClienteService cs = context.getBean(ClienteService.class);
+		cs.inserirCliente(cli);
+		return "pedido";
 	}
 	
 	@GetMapping("/sobre")
 	public String sobre() {
 		return "sobre";
 	}
+	
 }
